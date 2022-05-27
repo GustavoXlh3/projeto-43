8a5e7fed671a3c4c89a3898270a1f15466a3295b
GustavoXlh3 <92042866+GustavoXlh3@users.noreply.github.com>
2022-05-27 20:55:10 -0300

    docs: add files

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "BINARIO";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("I R B N I O A", 100,50)
+    fill("lightblue")
+    text("Dica: 0 ou 1 nunca 2", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("Ç U N F O Ã", 700,150)
+    fill("lightblue")
+    text("Dica: Performa uma tarefa em particular!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("B N D A O C A S D O E D", 100,250)
+    fill("lightblue")
+    text("Dica: Armazena todas as informações!", 100,270)
+
+}
